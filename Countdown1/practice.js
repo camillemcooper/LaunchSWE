c0536cc5571b1ca65521b5a6fe910e7704ee37c0
Angela Gagin <afg4pr@virginia.edu>
2021-03-13 14:35:01 -0500

    Update practice.js

--- Countdown1/practice.js
+++ Countdown1/practice.js
@@ -12,24 +12,88 @@ const bookList = [
   { title: "Moby Dick", author: "Herman Melville" },
 ];
 
+const newEntry = (listOfBooks, newElement) => listOfBooks.push(newElement); // problem 1
+const removeEntry = (listOfBooks, elementIndex) => listOfBooks.splice(elementIndex, 1); // problem 2
+const printAll = (listOfBooks) => listOfBooks.forEach((obj) => console.log(obj.title)); // problem 3
+
+
 /* (4) Write a one-line arrow function that takes in a number and returns a string stating whether the
   number is positive or negative using a ternary operator (assume the number will never be zero) */
+
+const posNeg = (num) => let result = num > 0 ? "positive" : "negative"; // problem 4
 
 /* (5) Write a switch statement for a 'day' variable that prints out something based off of what day of
   the week it is
   i.e. if it's Monday, print "good luck" or wednesday print "hump day" or friday print "party" */
 
+const getDay = (day) => {
+  switch (day) {
+    case "Monday":
+      console.log("good luck");
+      
+    case "Tuesday":
+      console.log("Taco Tuesday!");
+      
+    case "Wednesday":
+      console.log("hump day");
+      
+    case "Thursday":
+      console.log("Almost there!");
+      
+    case "Friday":
+      console.log("party!!!");
+      
+    case "Saturday":
+      console.log("day off");
+      
+    case "Sunday":
+      console.log("New week!");
+  }
+}; // problem 5
+
 /* (6) Write an arrow function that takes in a number, and uses a for loop to return the sum of every
   number from 1 up to that number
   ex. sumUp(7) = 28 */
+const summation = (num) => {
+  let result = 0;
+  for (x = 1; x <= num; x++){
+    result = result + x;
+  }
+};
 
 /* (7) Write an arrow function that converts the temperature from Celsius to Fahrenheit and then tells
   me what I should wear accordingly */
+const temperature = (celsius) => {
+  const fahrenheit = (celsius * 1.8) + 32;
+  if (fahrenheit < 32){
+    console.log("Wear thick layers! Brrr!");
+  }
+  
+  if (fahrenheit < 50){
+    console.log("Wear a sweater!");
+  }
+  
+  if (fahrenheit < 60) {
+    console.log("Wear a longsleeved shirt.");
+  }
+  
+  if (fahrenheit < 90) {
+    console.log("Wear a t-shirt.");
+  }
+  
+  if (fahrenheit > 90) {
+    console.log("It's too hot!");
+  }
+}; // problem 7
 
 /* (8) Write a function that takes in an array and prints out the amount of truthy values in that array
   using .forEach() */
 
 const exampleArray = ["Hello, world!", 8, null, false, "", "0", -22];
+
+const truthy = (exArray) => {
+  exArray.forEach((item) => let result = item ? result + 1 : result));
+}; // problem 8
 
 /* (9) Using the map function and arrow syntax, return an array of object that contain a fullName field
   and an averageGrade field representing the letter grade that corresponds to their GPA */
@@ -43,6 +107,15 @@ const attendance = [
   { firstName: "Jackson", lastName: "Wolf", gpa: 4.0 },
   { firstName: "Jared", lastName: "Nguyen", gpa: 4.0 },
 ];
+
+attendance.map((person) => (
+  <fullName key = {person.lastName} value = {person.firstname + " " + person.lastname}>
+    {person.lastname + " " + person.lastname}
+  </fullName>
+  
+  <averageGrade key = {person.lastName} value = {person.gpa}>
+  {pe
+  ))}
 
 /* (10) Write a function that solves the "every number eventually equals 4" puzzle. The output should be
   an array of the path you took to make it equal four
