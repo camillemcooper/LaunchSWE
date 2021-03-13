5819b621c149a775d6b8a39bacd46cf9504bd652
kevingrieco <kevingrieco@d-172-26-66-66.dhcp.virginia.edu>
2021-03-13 14:18:37 -0500

    Completed numbers 1-7 of the activity

--- Countdown1/practice.js
+++ Countdown1/practice.js
@@ -12,24 +12,103 @@ const bookList = [
   { title: "Moby Dick", author: "Herman Melville" },
 ];
 
+// (1) Add new entry to bookList
+addBook = (title, author) => { 
+  const item =  {
+    title: title,
+    author: author
+  }
+  bookList.push(item)
+}
+
+// (2) Remove book from bookList
+removeBook = (index) => {bookList.splice(index, 1)}
+
+// (3) List out books or authors in bookList
+displayBooks = (type) => {
+  for (i = 0; i<bookList.length; i++) {
+    console.log(bookList[i][type]);
+  }
+}
+
+// Testing
+removeBook(0);
+addBook("The Perks of Being a Wallflower", "Stephen Chbosky");
+displayBooks('title');
+displayBooks('author');
+
 /* (4) Write a one-line arrow function that takes in a number and returns a string stating whether the
   number is positive or negative using a ternary operator (assume the number will never be zero) */
+
+  let posNeg = (number) => {let result = number > 0 ? "The number is positive." : "The number is negative."; return result;}
+
+  // Testing
+  console.log(posNeg(3));
+  console.log(posNeg(-5));
 
 /* (5) Write a switch statement for a 'day' variable that prints out something based off of what day of
   the week it is
   i.e. if it's Monday, print "good luck" or wednesday print "hump day" or friday print "party" */
 
+  let day = "Monday";
+  // Testing
+  day = "Monday"
+  day = "Wednesday"
+  day = "na"
+
+  switch (day) {
+    case "Monday":
+      console.log("Good luck");
+      break;
+    case "Tuesday":
+      console.log("Keep pushing");
+      break;
+    case "Wednesday":
+      console.log("Hump day");
+      break;
+    case "Thursday":
+      console.log("Almost there");
+      break;
+    case "Friday":
+      console.log("Party");
+      break;
+    case "Saturday":
+      console.log("Woohoo");
+      break;
+    case "Sunday":
+      console.log("Get some work done");
+      break;
+    default:
+      console.log("What day is it?");
+  }
+
+  
 /* (6) Write an arrow function that takes in a number, and uses a for loop to return the sum of every
   number from 1 up to that number
   ex. sumUp(7) = 28 */
 
+  sumUp = (number) => {let sum = 0; for (i=1;i<=number;i++){sum += i;} return sum;}
+  //Testing
+  console.log(sumUp(7));
+  console.log(sumUp(3));
+
 /* (7) Write an arrow function that converts the temperature from Celsius to Fahrenheit and then tells
   me what I should wear accordingly */
+  celToFahr = (temperature) => {let fahr = temperature*(9/5) + 32; let statement = fahr > 50 ? "Wear some sunblock!" : "Grab your winter coat!"; return statement;}
+
+  //Testing
+  console.log(celToFahr(30));
+  console.log(celToFahr(0));
 
 /* (8) Write a function that takes in an array and prints out the amount of truthy values in that array
   using .forEach() */
 
 const exampleArray = ["Hello, world!", 8, null, false, "", "0", -22];
+
+/*
+function numTruthy(array) {
+  array.forEach((value) => {let result === value})
+}*/
 
 /* (9) Using the map function and arrow syntax, return an array of object that contain a fullName field
   and an averageGrade field representing the letter grade that corresponds to their GPA */
