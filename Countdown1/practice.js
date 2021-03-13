3c499a6c68002fe899d6182fc27307187510b27b
jmg2akg <80379376+jmg2akg@users.noreply.github.com>
2021-03-13 14:14:35 -0500

    Update practice.js

--- Countdown1/practice.js
+++ Countdown1/practice.js
@@ -4,14 +4,71 @@ Using the following array of objects,
 (1) Write an arrow function that adds a new entry to bookList
 (2) Write an arrow function that removes a specific book from the bookList
 (3) Write an arrow function that lists out all the books or all the authors in the book list */
-
+const addNewEntry = (title, author) => bookList.push({title: title, author: author})
+const removeBook = (title, author) => bookList.splice(bookList.indexOf({title: title, author: author}), 1)
+const listAllBooks = () => {
+  for(i = 0; i < bookList.length; i++)
+      console.log(bookList[i]);
+}
 const bookList = [
   { title: "Don Quixote", author: "Miguel de Cervantes" },
   { title: "Ulysses", author: "James Joyce" },
   { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
   { title: "Moby Dick", author: "Herman Melville" },
 ];
+addNewEntry("a", "b");
+console.log(bookList);
+removeBook("a", "b");
+console.log(bookList);
+listAllBooks();
 
+const negOrPos = (num) => num > 0 ? "positive" : "negative"
+console.log(negOrPos(7));
+console.log(negOrPos(-7));
+
+const day = "Tuesday"
+
+switch(day) {
+  case "Monday":
+    console.log("Mon");
+    break;
+  case "Tuesday":
+    console.log("Tue");
+    break;
+  case "Wednesday":
+    console.log("Wed");
+    break;
+  case "Thursday":
+    console.log("Thu");
+    break;
+  case "Friday":
+    console.log("Fri");
+    break;
+  case "Saturday":
+    console.log("Sat");
+    break;
+  case "Sunday":
+    console.log("Sun");
+    break;
+  default:
+    console.log("Not a day of the week")
+}
+
+const sumUp = (num) => {
+  sum = 0;
+  for(i = 1; i < num+1; i++)
+    sum += i;
+  return sum;
+}
+console.log(sumUp(7));
+
+const convertTemp = (celsius) => {
+  f = celsius*9.0/5.0+32.0;
+  m = f > 65 ? "Wear a t shirt" : "Wear a jacket";
+  console.log(m);
+}
+
+convertTemp(20);
 /* (4) Write a one-line arrow function that takes in a number and returns a string stating whether the
   number is positive or negative using a ternary operator (assume the number will never be zero) */
 
@@ -28,12 +85,37 @@ const bookList = [
 
 /* (8) Write a function that takes in an array and prints out the amount of truthy values in that array
   using .forEach() */
+const numTruthy = (array) => {
+  sum = 0;
+  const isTruthy = (v) => {
+    sum = v ? sum+1 : sum
+  }
 
+  array.forEach(isTruthy);
+  return sum;
+}
 const exampleArray = ["Hello, world!", 8, null, false, "", "0", -22];
+console.log(numTruthy(exampleArray));
 
+const convertObj = (array) => {
+  const createEntry = (obj) => {
+    const n = obj["gpa"];
+    let letter = "A";
+    if (n > 3.0)
+      letter = "A";
+    else if (n > 2.0)
+      letter = "B";
+    else if (n > 1.0)
+      letter = "C";
+    else
+      letter = "D";
+    return {fullname: obj["firstName"] + " " + obj["lastName"], lettergrade: letter};
+  }
+  const newArr = array.map(createEntry);
+  return newArr;
+}
 /* (9) Using the map function and arrow syntax, return an array of object that contain a fullName field
   and an averageGrade field representing the letter grade that corresponds to their GPA */
-
 const attendance = [
   { firstName: "Clay", lastName: "Tondreau", gpa: 4.0 },
   { firstName: "Tucker", lastName: "Wilson", gpa: 2.0 },
@@ -43,7 +125,11 @@ const attendance = [
   { firstName: "Jackson", lastName: "Wolf", gpa: 4.0 },
   { firstName: "Jared", lastName: "Nguyen", gpa: 4.0 },
 ];
+console.log(convertObj(attendance));
 
+const solveFour = (num) => {
+  let num2 = len
+}
 /* (10) Write a function that solves the "every number eventually equals 4" puzzle. The output should be
   an array of the path you took to make it equal four
   ex/ [11, 6, 3, 5, 4], [19, 8, 5, 4] or [252, 18, 8, 5, 4]
