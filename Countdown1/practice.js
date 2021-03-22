15311e60c0cf2a5bf927b06bbdf2ff55d49615b7
Rory Gudka <rory.gudka@gmail.com>
2021-03-21 23:03:37 -0400

    Adjusted the final question to unique code

--- Countdown1/practice.js
+++ Countdown1/practice.js
@@ -113,51 +113,29 @@ const nameAndGPA = list => {
   ex/ [11, 6, 3, 5, 4], [19, 8, 5, 4] or [252, 18, 8, 5, 4]
   For context: https://puzzling.stackexchange.com/questions/29137/every-number-eventually-equals-4 */
 
-//I found this online, I don't even know if its possible to do this without some backbreaking effort without using a premade function
-//References: https://gist.github.com/ForbesLindesay/5467742
-function humanize(num){
-  var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-              'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
-              'seventeen', 'eighteen', 'nineteen'];
-  var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
-              'ninety'];
-
-  var numString = num.toString();
-
-  if (num < 0) throw new Error('Negative numbers are not supported.');
-
-  if (num === 0) return 'zero';
-
-  //the case of 1 - 20
-  if (num < 20) {
+const numToText = num => {
+  const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  
+  if(num > 0 && num <= 20) {
     return ones[num];
   }
-
-  if (numString.length === 2) {
-    return tens[numString[0]] + ' ' + ones[numString[1]];
+  else if(num > 20 && num < 100) {
+    const first = tens[parseInt(('' + num)[0])];
+    const second = ones[parseInt(('' + num)[1])];
+    if(second != '') {
+      return first + " " + second;
+    }
+    return first;
   }
-
-  //100 and more
-  if (numString.length == 3) {
-    if (numString[1] === '0' && numString[2] === '0')
-      return ones[numString[0]] + ' hundred';
-    else
-      return ones[numString[0]] + ' hundred and ' + convert(+(numString[1] + numString[2]));
-  }
-
-  if (numString.length === 4) {
-    var end = +(numString[1] + numString[2] + numString[3]);
-    if (end === 0) return ones[numString[0]] + ' thousand';
-    if (end < 100) return ones[numString[0]] + ' thousand and ' + convert(end);
-    return ones[numString[0]] + ' thousand ' + convert(end);
-  }
+  return "zero";
 }
 
 const fourPuzzle = num => {
   let cur = num;
   let arr = [num];
-  while(humanize(cur).length != 4) {
-    cur = hunanize(cur).length;
+  while(numToText(cur).length != 4) {
+    cur = numToText(cur).length;
     arr.push(cur);
   }
   return arr;
