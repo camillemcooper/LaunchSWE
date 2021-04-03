a046a88926d42a6d20b9e4e360d530e3fdd3041f
TJ Vitchutripop <79555155+tjdawgzone@users.noreply.github.com>
2021-04-03 03:04:21 -0400

    Update practice.js

--- Countdown1/practice.js
+++ Countdown1/practice.js
@@ -5,12 +5,24 @@ Using the following array of objects,
 (2) Write an arrow function that removes a specific book from the bookList
 (3) Write an arrow function that lists out all the books or all the authors in the book list */
 
+const addBook = (book,bookList) => bookList.push(book);
+const removeBook = (index,bookList) => bookList.splice(index, 1);
+const listBooks = (bookList) => {
+  for(let x=0;x<bookList.length;x++){
+    console.log(bookList[x].title);
+  }
+};
+
 const bookList = [
   { title: "Don Quixote", author: "Miguel de Cervantes" },
   { title: "Ulysses", author: "James Joyce" },
   { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
   { title: "Moby Dick", author: "Herman Melville" },
 ];
+var book1 = {title:"Harry Potter and the Sorcerer's Stone", author:"JK Rowling"};
+addBook(book1, bookList);
+
+listBooks(bookList);
 
 /* (4) Write a one-line arrow function that takes in a number and returns a string stating whether the
   number is positive or negative using a ternary operator (assume the number will never be zero) */
@@ -29,7 +41,73 @@ const bookList = [
 /* (8) Write a function that takes in an array and prints out the amount of truthy values in that array
   using .forEach() */
 
+const posOrNeg = a => {
+  const result = a>0 ? "positive" : "negative";
+};
+
+const day = 'Monday';
+
+switch(day){
+  case 'Monday':
+    console.log('good luck');
+    break;
+  case 'Tuesday':
+    console.log('keep going');
+    break;
+  case 'Wednesday':
+    console.log('hump day');
+    break;
+  case 'Thursday':
+    console.log('almost there');
+    break;
+  case 'Friday':
+    console.log('party');
+    break;
+  case 'Saturday':
+    console.log('woohoooooooo');
+    break;
+  case 'Sunday':
+    console.log('oooooooooo oh no its Monday tomorrow');
+    break;
+  default:
+    console.log('This is not a day');
+  
+}
+
+const sumUp = x =>{
+  let sum = 0;
+  for(let y=0;y<=x;y++){
+    sum=sum+y;
+  }
+  return sum;
+};
+const b = sumUp(7);
+console.log(b); 
+
+const convertAndAdvice = (x) =>{
+  x = (x*(9/5))+32
+  const y = x>50 ? "You don't need a jacket. The temperature is "+x+" degrees F." : "You need a jacket. The temperature is "+x+" degrees F.";
+  return y;
+};
+
+const test = convertAndAdvice(0);
+console.log(test);
 const exampleArray = ["Hello, world!", 8, null, false, "", "0", -22];
+
+const numTruthy = (a) =>{
+  let sum = 0;
+  const countTruthy = (b) =>{
+    if(b){
+      sum=sum+1;
+      
+    }
+  };
+  a.forEach(countTruthy);
+  return sum;
+};
+console.log(numTruthy(exampleArray));
+
+
 
 /* (9) Using the map function and arrow syntax, return an array of object that contain a fullName field
   and an averageGrade field representing the letter grade that corresponds to their GPA */
@@ -43,6 +121,28 @@ const attendance = [
   { firstName: "Jackson", lastName: "Wolf", gpa: 4.0 },
   { firstName: "Jared", lastName: "Nguyen", gpa: 4.0 },
 ];
+
+const changeFields = (a) =>{
+  const gpaToGrade = (x) =>{
+    if(x>=3.7)
+      return "A";
+    else if(x>=2.7)
+      return "B";
+    else if(x>=1.7)
+      return "C";
+    else if(x>=1.0)
+      return "D";
+    else 
+      return "F";
+  };
+
+  let newArray = a.map((index)=>({fullName: attendance[index].firstName+attendance[index].lastName, 
+    averageGrade:gpaToGrade(thisArg.gpa)}));
+};
+changeFields(attendance);
+console.log(attendance);
+
+
 
 /* (10) Write a function that solves the "every number eventually equals 4" puzzle. The output should be
   an array of the path you took to make it equal four
